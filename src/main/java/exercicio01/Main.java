591c2120b9a30c886a02965d926b526001d54b72
João Augusto Câmara Lopes Costa <camarajoaoaugusto@gmail.com>
2025-09-04 21:35:43 -0300

    Iniciando exercicio

---
+++ src/main/java/exercicio01/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package exercicio01;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author camar
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Digite o ");
+    }
+}
